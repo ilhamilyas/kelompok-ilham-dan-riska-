fa6893dd93fbd6f2e5bd84fad9e5e19691b8be82
DESKTOP-II9ABME\ilham ilyas <ilhamilyas100@gmail.com>
2018-04-23 23:05:43 -0700

    pertama

---
+++ Selamatkan Sang Kekasih/pipabawah.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class pipabawah here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pipabawah extends Actor
+{
+    /**
+     * Act - do whatever the pipabawah wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+         setLocation(getX()-3,getY());
+
+  
+    }    
+}
