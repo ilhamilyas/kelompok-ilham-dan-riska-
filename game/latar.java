7f49f2208c78ab84d833e1543126f54ed1cab1ce
riska12 <riska0121295@gmail.com>
2018-05-16 10:30:40 +0700

    push r

--- game/latar.java
+++ game/latar.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+  import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 
 public class latar extends World
@@ -9,24 +9,35 @@ public class latar extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
 
-        super(600, 400, 1,false);
+        super(600, 400, 1,true);
         addObject(Skor,300,20);
-        
-              
+
         addObject (new pesawat(), 100,200);
-      
+        prepare();
     }
+
+
     public void act()
     {
         if(jeda>0)jeda--;
         else jeda=100;
         if(jeda==1){
-        int randomHeight=Greenfoot.getRandomNumber (getHeight());
-        addObject (new musuh(-(Greenfoot.getRandomNumber (90))), getWidth()+100,randomHeight);
+            int randomHeight=Greenfoot.getRandomNumber (getHeight());
+            addObject (new musuh(-(Greenfoot.getRandomNumber (90))), getWidth()+100,randomHeight);
         }
     }
+
     public void tambah(){
         Skor.add(5);
-    }     
-   
+    }  
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
 }
+   
+
--- game/pesawat.java
+++ game/pesawat.java
@@ -8,48 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class pesawat extends Actor
 {
-   private int jeda=0;
-   private boolean toRemove=false;
-   int skor;
-   
+    /**
+     * Act - do whatever the pesawat wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
     public void act() 
     {
-       
-             
-         tembak();
-        if (Greenfoot.isKeyDown("up")){
-        setLocation(getX(),getY()-5);
+        // Add your action code here.
+        mover(); //call a method
+    }    
+    public void mover()
+    {
+        if (Greenfoot.isKeyDown("Up"))
+        {
+            setLocation(getX(),getY()-10);
         }
-        if (Greenfoot.isKeyDown("down")){
-        setLocation(getX(),getY()+5);
-        }
-        if (Greenfoot.isKeyDown("left")){
-        setLocation(getX()-5,getY());
-        }
-        if (Greenfoot.isKeyDown("right")){
-        setLocation(getX()+5,getY());
-        }
-        //if(jeda>0)jeda--;
-        //if(jeda==1)getWorld().addObject(new peluru(), getX()+100,getY());
-        //if(jeda==0)jeda=5;
-   
-     if (getOneIntersectingObject(musuh.class)!=null){
-        Greenfoot.stop ();
-        getWorld().addObject(new gagal(),
-        getWorld().getWidth()/2,
-        getWorld().getHeight()/2);
-        
-        }
-
-    }
-   
-    public void tembak () {
-            if (Greenfoot.isKeyDown("space")){
-            if(jeda>0)jeda--;
-            if(jeda==1) getWorld().addObject(new peluru(),getX(),getY());
-            if(jeda==0)jeda=12;
-        }
-    }
-    
-
+        if (Greenfoot.isKeyDown("Down"))
+        {
+             setLocation(getX(),getY()+10);
+            }
+}
 }
